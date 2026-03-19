b786a872550f34e83b469835bf06dd01f44b04e2
Kroki <sonakonaeva288@gmail.com>
2026-03-19 22:23:43 +0300

    первый пробный тест

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -9,6 +9,11 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            int res = 2 + 2;
+            Assert.AreEqual(res, 4);
+            Assert.AreNotEqual(res, 5);
+            Assert.IsFalse(res > 5);
+            Assert.IsTrue(res < 5);
         }
     }
 }
